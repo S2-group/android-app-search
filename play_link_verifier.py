394bd545dbe4f4dc5b8f6de451a0fbf925c0fd77
Franz Geiger <mail@fx-g.de>
2017-10-10 19:54:56 +0200

    Add option to consider 403 status codes as available
    Google Play sometimes returns status code 403 for certain packages.
    Consider anything other than status code 200 as an indicator for
    availability.

--- play_link_verifier.py
+++ play_link_verifier.py
@@ -30,13 +30,22 @@ def parse_cmdline_arguments() -> argparse.Namespace:
     arguments.add_argument('--log', default=sys.stderr,
             type=argparse.FileType('w'),
             help='Log file. Default: stderr.')
+    arguments.add_argument('--include-403', action='store_true',
+            help='Include package names which Google Play returns '
+                'status `403 Unauthorized` for.')
     return arguments.parse_args()
 
 
-def is_package_in_play(package_name: str) -> bool:
+def is_package_in_play(package_name: str, include_403: bool) -> bool:
     """Test if package_name is available in Google Play.
 
+    Response code `200 Success` is considered an indicator of
+    availability. So is response code `403 Unauthorized` in case
+    include_403 is True.
+
     :param str package_name: Package name to search for in Google Play.
+    :param bool include_403: Consider packages valid that get a
+        response code `403 Unauthorized`.
     :returns bool: True if package name is available in Google Play,
         False otherwise.
     """
@@ -51,10 +60,12 @@ def is_package_in_play(package_name: str) -> bool:
     else:
         logger.info(log_msg)
 
-    return response.status_code == 200
+    return response.status_code == 200 or (include_403 and
+            response.status_code == 403)
 
 
-def package_filter(input_file: IO[str], output_file: IO[str]):
+def package_filter(input_file: IO[str], output_file: IO[str],
+        include_403=False):
     """Filter out lines if they do not exist in Google Play.
 
     :param IO[str] input_file: File to read lines from. Each line is
@@ -64,7 +75,7 @@ def package_filter(input_file: IO[str], output_file: IO[str]):
     """
     for line in input_file.readlines():
         package = line.strip()
-        if is_package_in_play(package):
+        if is_package_in_play(package, include_403):
             print(package, file=output_file)
 
 
@@ -84,4 +95,4 @@ def configure_logger(stream: IO[str]):
 if __name__ == '__main__':
     args = parse_cmdline_arguments()
     configure_logger(args.log)
-    package_filter(args.input, args.output)
+    package_filter(args.input, args.output, args.include_unauthorized)
